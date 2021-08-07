7c820e9d951e186d0cdcefc210455c7797c61d0e
liamd <liamdanielprice@gmail.com>
2021-08-08 08:35:41 +1000

    Initial commit

---
+++ src/main/java/au/com/skynetmc/skynetproxy/SkynetProxy.java
@@ -0,0 +1,16 @@
+package au.com.skynetmc.skynetproxy;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class SkynetProxy extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
