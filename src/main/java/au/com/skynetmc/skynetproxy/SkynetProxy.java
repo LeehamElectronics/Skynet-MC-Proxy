ac00d8483858b44ba78023c504594341f41b8c8c
liamd <liamdanielprice@gmail.com>
2021-08-22 12:33:44 +1000

    added authors and startup message

--- src/main/java/au/com/skynetmc/skynetproxy/SkynetProxy.java
+++ src/main/java/au/com/skynetmc/skynetproxy/SkynetProxy.java
@@ -7,10 +7,12 @@ public final class SkynetProxy extends Plugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        getLogger().info("has enabled");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
     }
+
 }
